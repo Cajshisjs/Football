41f5c5ed3d8e5768d22572d11e9de6463daa9736
Cajshisjs <chargediba@gmail.com>
2024-06-15 21:15:38 +0800

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,67 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "football rng", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+local Tab = Window:MakeTab({
+    Name = "Script",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+local runningRonaldo = false
+local runningMoney = false
+local runningPlayerValue = false
+
+Tab:AddToggle({
+    Name = "get prime ronaldo rarest card",
+    Default = false,
+    Callback = function(Value)
+        runningRonaldo = Value
+        while runningRonaldo do
+            local args = {
+                [1] = "skibidi toilet sigma",
+                [2] = game:GetService("ReplicatedStorage"):WaitForChild("Prime"):WaitForChild("Prime Ronaldo")
+            }
+            game:GetService("ReplicatedStorage"):WaitForChild("NewCardAdding"):FireServer(unpack(args))
+            wait(0.01)
+        end
+    end
+})
+
+Tab:AddToggle({
+    Name = "get money 🤑",
+    Default = false,
+    Callback = function(Value)
+        runningMoney = Value
+        while runningMoney do
+            local args = {
+                [1] = 9999999999
+            }
+            game:GetService("ReplicatedStorage"):WaitForChild("ClaimReward"):FireServer(unpack(args))
+            wait(0.01)
+        end
+    end
+})
+
+Tab:AddToggle({
+    Name = "get player value (don't go above 2.4t you will get detected)",
+    Default = false,
+    Callback = function(Value)
+        runningPlayerValue = Value
+        while runningPlayerValue do
+            game:GetService('ReplicatedStorage'):FindFirstChild('PlayerValue'):FireServer(999999999)
+            wait(0.01)
+        end
+    end
+})
+
+-- Credits Tab
+local CreditsTab = Window:MakeTab({
+    Name = "Credits",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+CreditsTab:AddLabel("Credits to Newplayer900")
+
+OrionLib:Init()
